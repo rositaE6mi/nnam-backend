de16bf3bb11fc3e735ce3a3bf2bc2e4b5e509d36
RositaE6mi <odilemvondo13@gmail.com>
2025-02-16 18:15:03 +0100

    project created

---
+++ src/main/java/com/logonedigital/Nnam/NnamApplication.java
@@ -0,0 +1,13 @@
+package com.logonedigital.Nnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logonedigital/Nnam/NnamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logonedigital.Nnam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
