c750fa8ad758fdf4ca97d42ee6c3b0ffb8133251
Bechir235 <Bechir>
2025-02-21 23:13:46 +0100

    a

--- src/main/java/com/logonedigital/Nnam/entities/Livraison.java
+++ src/main/java/com/logonedigital/Nnam/entities/Livraison.java
@@ -1,4 +1,41 @@
 package com.logonedigital.Nnam.entities;
 
-public class Livraison {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+
+public class Livraison implements Serializable {
+
+    @Serial
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer idLivraison;
+
+    private String adresseDestination;
+
+    private String dateDeLivraison;
+
+    private String etatDeLivraison;
+
+
+
+    @ManyToOne
+    private List<Livraison> livraisons = new ArrayList<>();
 }
