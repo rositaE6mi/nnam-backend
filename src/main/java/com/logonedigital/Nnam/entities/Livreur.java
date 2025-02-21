afb6bf516627e6b49cc9978e0d73dc2b7525cd48
Bechir235 <Bechir>
2025-02-21 23:16:06 +0100

    a

--- src/main/java/com/logonedigital/Nnam/entities/Livreur.java
+++ src/main/java/com/logonedigital/Nnam/entities/Livreur.java
@@ -1,4 +1,36 @@
 package com.logonedigital.Nnam.entities;
 
-public class Livreur {
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Livreur implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer idLivreur;
+
+    private String nom;
+
+    private String prenom;
+
+    private String telephone;
+
+
+    private List<Livreur> livreurs = new ArrayList<>();
+
 }
