ebd72e0744cdb4b9e8cd0cd4902b853ebf308707
Franck237-coding <francghistamba@gmail.com>
2025-03-04 00:15:24 +0100

    Revert "Merge branch 'lamairie' into Rosita"
    This reverts commit 678f9bd85d7ef2236b78ed1edc9ca60fa2bc20aa, reversing
    changes made to 9df48bc13e37ecc80ddfc36dcfa9a5ae06aee46e.

--- src/main/java/com/logonedigital/Nnam/repository/FactureRepo.java
+++
@@ -1,8 +0,0 @@
-package com.logonedigital.Nnam.repository;
-
-import com.logonedigital.Nnam.entities.Facture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FactureRepo extends JpaRepository< Facture, Integer> {
-
-}
