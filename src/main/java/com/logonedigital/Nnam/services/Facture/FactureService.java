aa33aa853237e27f8d096703069a7bf1ab1600cb
lamairie <lemairegilles11@gmail.com>
2025-03-03 23:11:30 +0100

    Merge remote-tracking branch 'origin/lamairie' into lamairie
    lamairie

---
+++ src/main/java/com/logonedigital/Nnam/services/Facture/FactureService.java
@@ -0,0 +1,26 @@
+package com.logonedigital.Nnam.services.Facture;
+
+import com.logonedigital.Nnam.entities.Facture;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface FactureService {
+
+    // 📌 Ajouter une facture
+    Facture addfacture(Facture facture);
+
+    // 📌 Obtenir une facture par ID
+    Optional<Facture> getFacture(Integer id);
+
+    // 📌 Lister toutes les factures
+    List<Facture> ListerFactures();
+
+    // 📌 Mettre à jour une facture
+    Facture Updatefacture(Integer id, Facture facture);
+
+    // 📌 Supprimer une facture
+    boolean DeleteFacture(Integer id);
+}
