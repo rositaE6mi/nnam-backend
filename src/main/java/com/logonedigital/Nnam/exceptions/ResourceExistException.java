d6ab1aff22de566e54721f7204f7fd23b59d2f1c
lamairie <lemairegilles11@gmail.com>
2025-04-05 18:19:18 +0100

    lamairie

--- src/main/java/com/logonedigital/Nnam/exceptions/ResourceExistException.java
+++
@@ -1,7 +0,0 @@
-package com.logonedigital.Nnam.exceptions;
-
-public class ResourceExistException extends RuntimeException{
-    public ResourceExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/logonedigital/Nnam/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.logonedigital.Nnam.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
